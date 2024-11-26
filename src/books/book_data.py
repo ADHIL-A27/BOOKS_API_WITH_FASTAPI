c5432fc54413dd58908dfc12c0f04a9caeab21c4
MOHAMMED ADHIL <122203370+ADHIL-A27@users.noreply.github.com>
2024-11-27 01:11:37 +0530

    first fastapi dev

---
+++ src/books/book_data.py
@@ -0,0 +1,93 @@
+
+books = [
+    {
+        "id": 1,
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "publisher": "J.B. Lippincott & Co.",
+        "published_date": "1960-07-11",
+        "page_count": 281,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "1984",
+        "author": "George Orwell",
+        "publisher": "Secker & Warburg",
+        "published_date": "1949-06-08",
+        "page_count": 328,
+        "language": "English"
+    },
+    {
+        "id": 3,
+        "title": "Pride and Prejudice",
+        "author": "Jane Austen",
+        "publisher": "T. Egerton",
+        "published_date": "1813-01-28",
+        "page_count": 432,
+        "language": "English"
+    },
+    {
+        "id": 4,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "publisher": "Charles Scribner's Sons",
+        "published_date": "1925-04-10",
+        "page_count": 218,
+        "language": "English"
+    },
+    {
+        "id": 5,
+        "title": "Moby Dick",
+        "author": "Herman Melville",
+        "publisher": "Harper & Brothers",
+        "published_date": "1851-10-18",
+        "page_count": 635,
+        "language": "English"
+    },
+    {
+        "id": 6,
+        "title": "War and Peace",
+        "author": "Leo Tolstoy",
+        "publisher": "The Russian Messenger",
+        "published_date": "1869",
+        "page_count": 1225,
+        "language": "Russian"
+    },
+    {
+        "id": 7,
+        "title": "The Catcher in the Rye",
+        "author": "J.D. Salinger",
+        "publisher": "Little, Brown and Company",
+        "published_date": "1951-07-16",
+        "page_count": 277,
+        "language": "English"
+    },
+    {
+        "id": 8,
+        "title": "The Lord of the Rings",
+        "author": "J.R.R. Tolkien",
+        "publisher": "George Allen & Unwin",
+        "published_date": "1954-07-29",
+        "page_count": 1178,
+        "language": "English"
+    },
+    {
+        "id": 9,
+        "title": "Jane Eyre",
+        "author": "Charlotte Brontë",
+        "publisher": "Smith, Elder & Co.",
+        "published_date": "1847-10-16",
+        "page_count": 500,
+        "language": "English"
+    },
+    {
+        "id": 10,
+        "title": "The Hobbit",
+        "author": "J.R.R. Tolkien",
+        "publisher": "George Allen & Unwin",
+        "published_date": "1937-09-21",
+        "page_count": 310,
+        "language": "English"
+    }
+]
